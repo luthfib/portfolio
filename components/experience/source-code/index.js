5bdfa7656c5cde6152cfc806fdb2791404bfb35c
luthfib <luthfibustillos@gmail.com>
2022-02-06 14:40:32 -0500

    add link to source code

---
+++ components/experience/source-code/index.js
@@ -0,0 +1,9 @@
+import styles from "./source-code.module.scss";
+
+export const SourceCode = () => (
+  <div className={styles.sourceCode}>
+    <a target="_blank" href="https://github.com/luthfib/portfolio">
+      Source
+    </a>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import { Experience } from "../components/experience";
 import Footer from "../components/footer";
 import { Intro } from "../components/intro";
 import { Projects } from "../components/projects";
+import { SourceCode } from "../components/experience/source-code";
 
 export default function Home() {
   return (
     <div className="app">
+      <SourceCode />
       <Intro />
       <Projects />
       <Experience />
