608c576b9b7a3a079085ed81fbc50d2e898eab02
luthfib <luthfibustillos@gmail.com>
2022-02-06 21:29:29 -0500

    add animation to education

--- components/education/index.js
+++ components/education/index.js
@@ -1,11 +1,18 @@
 import styles from "./education.module.scss";
+import { useAnimateOnViewport } from "../../lib/hooks";
+import { useRef } from "react";
 
-export const Education = () => (
-  <section className={styles.education}>
-    <h1>Education</h1>
-    <div>
-      <h2>BS in Information Science</h2>
-      <h3>University of North Carolina at Chapel Hill</h3>
-    </div>
-  </section>
-);
+export const Education = () => {
+  const ref = useRef(null);
+  useAnimateOnViewport(ref);
+
+  return (
+    <section className={styles.education}>
+      <h1>Education</h1>
+      <div ref={ref}>
+        <h2>BS in Information Science</h2>
+        <h3>University of North Carolina at Chapel Hill</h3>
+      </div>
+    </section>
+  );
+};
