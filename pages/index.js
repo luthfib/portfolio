9b12b4f52628807941377ea18c4652158aaff81b
luthfib <luthfibustillos@gmail.com>
2022-02-07 09:35:16 -0500

    slight updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Education } from "../components/education";
 import { Experience } from "../components/experience";
 import Footer from "../components/footer";
+import Head from "next/head";
 import { Intro } from "../components/intro";
 import { Projects } from "../components/projects";
 import { SourceCode } from "../components/experience/source-code";
@@ -8,6 +9,10 @@ import { SourceCode } from "../components/experience/source-code";
 export default function Home() {
   return (
     <div className="app">
+      <Head>
+        <title>Luthfi Bustillo's Portolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <SourceCode />
       <Intro />
       <Projects />
